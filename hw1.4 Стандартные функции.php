2eb0ec391001affb190e5c0b2c2da5ec69d05f19
elmira2705 <37979137+elmira2705@users.noreply.github.com>
2018-06-08 01:46:32 +0600

    Update hw1.4 Стандартные функции.php

--- hw1.4 Стандартные функции.php
+++ hw1.4 Стандартные функции.php
@@ -1,36 +1,69 @@
 <?php
 
 $json = file_get_contents('http://api.openweathermap.org/data/2.5/weather?id=1526395&APPID=8998437e66f99c1bb088eb04b0eeea53');
-$object = json_decode($json, true);
-// print_r ($object);
-$objArray = (array) $object;
+if ($json === false) {
+  exit('Не удалось получить данные');
+}
 
-// echo "<pre>";
-// print_r($objArray);
-// echo "</pre>";
+if (empty($json)) {
+        echo 'json пуст';
+    }
 
-$temperature = $objArray["main"]["temp"];
-$pressure = $objArray["main"]["pressure"];
-$humidity = $objArray["main"]["humidity"];
-$weather_pic = $objArray["weather"]["main"];
+$objArray = (array) json_decode($json, true)
+if ($object === null) {
+  exit('Ошибка декодирования json');
+}
 
-echo '<h3>'."Погода в Алматы на сегодня:" .'</h3>';
+
+$temperature = $objArray['main']['temp'];
+$pressure = $objArray['main']['pressure'];
+$humidity = $objArray['main']['humidity'];
+$weather_pic = $objArray['weather'][0]['main'];
+
 $temperature_res = (int)($temperature/17);
+
 if ($temperature_res > 0){
-    echo '<i>' . "Температура: +" . '</i>' . $temperature_res . 'C°'.'<br>';
+    $temp = '+' . $temperature_res;
 } elseif ($temperature_res == 0) {
-    echo '<i>' . "Температура: 0 C°" . '</i>' .'<br>';
+    $temp = '0';
 } else  {
-    echo '<i>' . "Температура: -" . '</i>' . $temperature_res . 'C°'.'<br>';
+    $temp = '-' . $temperature_res;
 }
-echo '<i>' . "Влажность:" . '</i>' . $humidity . '%'.'<br>';
-echo '<i>' . "Атмосферное давление:" . '</i>' . $pressure . 'мм рт. ст.' . '<br>';
-if ($weather_pic == "Cloud") {
-    echo 'Облачно'.'<img src="https://u.cubeupload.com/Piligrimo/e326cf.jpg"><br>';
+
+if ($weather_pic == 'Cloud') {
+    $picture = print 'Облачно'.'<img src="http://openweathermap.org/img/w/04n.png"><br>';
 }   elseif ($weather_pic == "Rain") {
-    echo 'Дождь'.'<img src="https://u.cubeupload.com/Piligrimo/f303e7.jpg"><br>';
+    $picture = 'Дождь'.'<img src="http://openweathermap.org/img/w/09d.png"><br>';
 }   elseif ($weather_pic == "Mist") {
-    echo 'Туман'.'<img src="https://u.cubeupload.com/Piligrimo/cloudweathermistfog.png"><br>';
+    $picture = 'Туман'.'<img src="http://openweathermap.org/img/w/50d.png"><br>';
 }   else  {
-    echo 'Ясно'.'<img src="https://u.cubeupload.com/Piligrimo/c57771.jpg"><br>';
+    $picture = 'Ясно'.'<img src="http://openweathermap.org/img/w/01d.png"><br>';
 }
+?>
+<!DOCTYPE html>
+<html lang="ru">
+  <head>
+     <meta charset="UTF-8">
+     <title>1.4. Стандартные функции</title>
+  </head>
+  <body>
+    <h3>Погода в Алматы на сегодня: </h3>
+    <table>
+      <thead>
+        <tr>
+          <td>Температура</td>
+          <td>Влажность</td>
+          <td>Атмосферное давление</td>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td><i><?php echo $temp . 'C°'?></i></td>
+          <td><i><?php echo $humidity . '%'; ?></i></td>
+          <td><i><?php echo $pressure . 'мм рт. ст.'; ?></i></td>
+        </tr>
+      </tbody>
+    </table>
+    <?php echo $picture; ?><
+  </body>
+</html>
